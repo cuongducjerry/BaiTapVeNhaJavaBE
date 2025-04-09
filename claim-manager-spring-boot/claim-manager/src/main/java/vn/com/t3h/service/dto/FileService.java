eb40958704f67f163ca2cbab22c9640f742c45a2
Nguyen Duc Cuong <cuongducjerry@gmail.com>
2025-04-09 14:37:49 +0700

    JAVABE2411

--- claim-manager-spring-boot/claim-manager/src/main/java/vn/com/t3h/service/dto/FileService.java
+++
@@ -1,5 +0,0 @@
-package vn.com.t3h.service.dto;
-
-public interface FileService {
-    String getBase64FromPath(String path);
-}
--- claim-manager-spring-boot/claim-manager/src/main/java/vn/com/t3h/utils/Constant.java
+++
@@ -1,5 +0,0 @@
-package vn.com.t3h.utils;
-
-public class Constant {
-    public static String ROLE_ADMIN_CODE = "ADMIN";
-}
